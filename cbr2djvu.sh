4cca551a12f5f61da9f6a77c4421fb2eb8ae8305
technoheretic <technoheretic@gmail.com>
2025-02-19 14:50:52 -0300

    fix

--- cbr2djvu.sh
+++ cbr2djvu.sh
@@ -8,9 +8,6 @@ fi
 
 tmpdir=$(mktemp -d)
 
-echo "tmpdir=$tmpdir"
-
-
 unrar e "$(dirname "$0")"/"$1" "$tmpdir"
 for x in "$tmpdir"/*.jpg; do c44 -dpi 300 -slice 72+11+10+10 -bpp 0.25,0.5,1 $x ; done
 djvm -c "$(dirname "$0")"/"$1".djvu "$tmpdir"/*.djvu
